3c936b969e6abeba11bb2f2f83262ee9aa61dbb0
Carcraftz <40304589+Carcraftz@users.noreply.github.com>
2020-10-28 15:26:08 -0400

    Add files via upload

---
+++ server.js
@@ -0,0 +1,48 @@
+// server.js
+// where your node app starts
+
+// init project
+var express = require('express');
+var app = express();
+
+// we've started you off with Express, 
+// but feel free to use whatever libs or frameworks you'd like through `package.json`.
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get('/', function(request, response) {
+  response.sendFile(__dirname + '/views/index.html');
+});
+
+app.get('*', function(req, res){
+  res.sendFile(__dirname + '/views/404.html');
+});
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT, function() {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
+
+/*
+var express = require('express')
+var app = express()
+
+//force https so we can keep our ssl certificate
+function checkHttps(req, res, next){
+  // protocol check, if http, redirect to https
+  
+  if(req.get('X-Forwarded-Proto').indexOf("https")!=-1){
+    console.log("https, yo")
+    return next()
+  } else {
+    console.log("just http")
+    res.redirect('https://' + req.hostname + req.url);
+  }
+}
+
+app.all('*', checkHttps)
+app.use(express.static('public'))
+app.listen(process.env.PORT)
+*/
