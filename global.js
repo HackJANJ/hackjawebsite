12ff5000596e913c85e5a00b6966ff7b69d919fe
MrGorbunov <volarcko@gmail.com>
2020-12-13 16:09:06 -0500

    Added footer to all pages (about.html is bad)
    BRO Abouat.html's footer is so weird. It won't render correctly unlike everything else

--- global.js
+++ global.js
@@ -8,12 +8,57 @@ useful elements to repeat. This script does that
 function updateAllFooters () {
   var footersToUpdate = document.getElementsByClassName("update-this-footer");
 
-  // for (let i=0; i<footersToUpdate.length; i++) {
-  //   let elem = footersToUpdate[i];
-  //   elem.innerHTML += `
-  //     <p>\"No way\" Not possible</p>
-  //   `;
-  // }
+  for (let i=0; i<footersToUpdate.length; i++) {
+    let elem = footersToUpdate[i];
+    elem.innerHTML += `
+    <div class="container my-5">
+
+      <!-- Sponsor images -->
+      <div class="card text-center">
+        <div class="card-header">
+            <p class="lead text-mono text-success my-2">
+              HackJA would not be possible without our <b><i>amazing</i></b> sponsors.
+            </p>
+        </div>
+
+        <div class="card-body">
+          <br>
+          <br>
+          <div class="row justify-content-md-center">
+            <div class="col">
+              <img class="footer-sponsor-img" src="https://cdn.glitch.com/9997fac9-1a66-40ea-9959-2e849e187dcf%2Fadp-red-logo-w-tag-rgb-center-updated_orig.png?1547959399369" alt="ADP">
+            </div>
+            <div class="col">
+              <img class="footer-sponsor-img" src="https://cdn.glitch.com/9997fac9-1a66-40ea-9959-2e849e187dcf%2Fmlh-logo-color.png?1547959659484" alt="Major League Hacking">
+            </div>
+            <div class="col">
+              <img class="footer-sponsor-img" src="https://cdn.glitch.com/9997fac9-1a66-40ea-9959-2e849e187dcf%2FBayerFund_Reversed-Color-for-dark-backgrounds.png?v=1607818504955" alt="Bayer">
+            </div>
+          </div>  
+        </div>
+      </div>
+
+      <!-- JANJ Content -->
+      <div>
+        <br>
+        <br>
+        <div class="row align-items-center justify-content-center">
+            <div style="display: flex;">
+              <div>
+                <span>
+                  HackJA &copy; Copyright 2020<br />
+                  An event run by Junior Achievement of New Jersey
+                </span>
+              </div>
+
+              <img class="mx-5" style="width: auto; height: auto;" alt="Junior Achievement USA, a member of JA worldwide" src="https://cdn.glitch.com/9997fac9-1a66-40ea-9959-2e849e187dcf%2FJanj_logo.png?v=1607889948448">
+            </div>
+        </div>
+
+      </div>
+    </div>
+    `;
+  }
 }
 
 updateAllFooters();
