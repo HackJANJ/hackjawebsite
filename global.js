fb87f70ca871424bd48903618255659754943560
Carcraftz <40304589+Carcraftz@users.noreply.github.com>
2020-12-29 18:27:43 -0500

    Update global.js

--- global.js
+++ global.js
@@ -35,7 +35,7 @@ function updateAllFooters () {
               <img style="max-width:75%" class="footer-sponsor-img" src="https://launchx.com/assets/images/launchXLogo.png" alt="LaunchX">
             </div>
       <div class="col">
-              <img style="max-width:80%" class="footer-sponsor-img" src="https://static.wixstatic.com/media/c42fea_d1fbddac9a524f16b3d4456d639b444a~mv2.png/v1/fill/w_750,h_375,al_c/c42fea_d1fbddac9a524f16b3d4456d639b444a~mv2.png" alt="EchoAR">
+              <img style="max-width:80%" class="footer-sponsor-img" src="https://static.wixstatic.com/media/c42fea_9d1970a5afdd47978ba9646a72c6dc1e~mv2.png/v1/fill/w_286,h_104,al_c,q_85,usm_0.66_1.00_0.01/echoAR%20-%20Logo%202020%20-%20Knockout.webp" alt="EchoAR">
             </div>
           </div>  
         </div>
@@ -54,7 +54,7 @@ function updateAllFooters () {
               </div>
 
               <div class="col col-lg-4 col-md-5 col-sm-8">
-                <img class="mx-5" style="max-width: 100%; width: auto; height: auto;" alt="Junior Achievement USA, a member of JA worldwide" src="https://cdn.glitch.com/9997fac9-1a66-40ea-9959-2e849e187dcf%2FJanj_logo.png?v=1607889948448">
+                <img class="mx-5" style="max-width: 100%; width: auto; height: auto;" alt="Junior Achievement NJ, a member of JA worldwide" src="https://janj.ja.org/contentAsset/image/38554408-7871-446e-8027-a113b71504d7/hostThumbnail/filter/Resize/resize_h/80">
               </div>
         </div>
 
